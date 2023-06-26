7547bf9e3795a7f47937b9a5b5e3ed72ab32f261
Nick <nicksweetj@gmail.com>
2023-06-26 12:45:40 -0400

    inital test push with starter code

---
+++ index.js
@@ -0,0 +1,46 @@
+const inquirer = require("inquirer");
+
+const questions = [
+    {
+        name: "abbr",
+        message: "What is the abbreviation (3 letters) that you would like to use for this logo?" 
+    },
+    {
+        name: "color",
+        message: "What color would you like your logo to be? (Please use color keyword or hexidecimal code)"
+    },
+    {
+        name: "shape",
+        message: "What shape would you like the text to be?",
+        type: "list",
+        choices: ["Circle", "Triangle", "Square"]
+    },
+    {
+        name: "shapeColor",
+        message: "What color would you like the shape to be? (Please use color keyword or hexidecimal code)"
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (error) => {
+        if (error) {
+            console.log("error"); // logs error if error occurs
+        } else {
+            console.log("Logo."); //logs proof that file has been created
+        }
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers);
+        const markdownContent = generateMarkdown(answers); //stoes the data to enter into README file to a variable
+        writeToFile("logo.svg", markdownContent); //calls the function to actually generate the README file
+    });
+};
+
+// Function call to initialize app
+init();
